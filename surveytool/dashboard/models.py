d2372b23d8a0259fece43bd6dc7c6a6fc0564fd1
harii <heyhari129@gmail.com>
2014-06-14 14:46:19 -0400

    Made Farmer data model and 6 sample farmers. Use function in views.py to get the sample data for the map.

--- surveytool/dashboard/models.py
+++ surveytool/dashboard/models.py
@@ -1,11 +1,24 @@
 from django.db import models
- 
 
 class Farmer(models.Model):
     name = models.CharField(max_length=50)
+    land_area = models.FloatField(default=0.0) # radius for now
+    latitude = models.FloatField(default=0.0)
+    longitude = models.FloatField(default=0.0)
+    is_diseased = models.BooleanField(default=False)
+
+    def __str__(self):
+    	return str({'name':self.name,
+    	'land_area':self.land_area,
+    	'latitude':self.latitude,
+    	'longitude':self.longitude,
+    	'is_diseased':self.is_diseased})
+
+class RealFarmer(models.Model):
+    name = models.CharField(max_length=50)
     address = models.CharField(max_length=250)
-    land_area = models.FloatField(default=0.0)
+    land_area = models.FloatField(default=0.0) # radius for now
     water_area = models.FloatField(default=0.0)
     reg_number = models.CharField(max_length=50)
     farm_location = models.CharField(max_length=250)
-    phone_number = models.CharField(max_length=11)	
+
--- surveytool/dashboard/views.py
+++ surveytool/dashboard/views.py
@@ -1,8 +1,53 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from dashboard.models import Farmer
+import random, json
+
+def generateSampleData():
+    sample_names = ("Bob", "Dan", "Chloe", "Lyra", "Dev", "Eric")
+    sample_land_area = (100, 140, 120, 30, 10, 1500)
+    sample_lat =         (20.762765 + random.random()/1000,
+        20.762765 + random.random()/1000,
+        20.762765 + random.random()/1000,
+        20.762765 + random.random()/1000,
+        20.762765 + random.random()/1000,
+        20.762765 + random.random()/1000)
+    sample_long =     (72.978160 + random.random()/1000,
+    72.978160 + random.random()/1000,
+    72.978160 + random.random()/1000,
+    72.978160 + random.random()/1000,
+    72.978160 + random.random()/1000,
+    72.978160)
+    sample_diseased = (True, False, False, False, True, True)
+    sample_farmers = []
+
+    for i in range(6):
+        name=sample_names[i]
+        land_area=sample_land_area[i],
+        latitude=sample_lat[i], 
+        longitude=sample_long[i],
+        is_diseased=sample_diseased[i]
+        sample_farmers.append(Farmer(name=name, land_area=land_area,
+                                     latitude=latitude, longitude=longitude,
+                                     is_diseased=is_diseased))
+    return sample_farmers
+
+def returnSampleFarmerData():
+    farmers = generateSampleData()
+    data = {'all_farmers':[]}
+    for item in farmers:
+        data['all_farmers'].append(str(item))
+    response = json.dumps(data)
+    # print json.loads(response)
+    # print json.loads(response)['all_farmers']
+    print json.loads(response)['all_farmers'][0]
+    return json.dumps(data)
+
 
 def dashboard(request):
 
-    context = {'json', json}
+    returnSampleFarmerData()
+
+    context = ""
     return render(request, 'dashboard/dashboard.html', context)
 
