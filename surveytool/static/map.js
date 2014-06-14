ab8318eb2e0ce6b26e401f1664f3353c477ca953
Maura Hausman <mdhausman@gmail.com>
2014-06-14 17:47:21 -0400

    added disease info

--- surveytool/static/map.js
+++ surveytool/static/map.js
@@ -4,7 +4,8 @@ var mapOptions = {
 };
 var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 
-var markers = []
+var markers = [];
+var mc;
 
 $.ajax({
 	type: "GET",
@@ -21,17 +22,20 @@ function populateData(data){
 		var lat = parseFloat(this.fields.latitude.replace(/[^0-9.]+/g, ''));
 		var lng = parseFloat(this.fields.longitude.replace(/[^0-9.]+/g, ''));
 		markers[i] = new google.maps.Marker({position: new google.maps.LatLng(lat,lng)});
-		i++;
+
+		var contentString = "Disease Presence: " + this.fields.is_diseased;
+
+		var infowindow = new google.maps.InfoWindow({
+		    content: contentString
+		});
+
+		google.maps.event.addListener(marker, 'click', function() {
+		    infowindow.open(map,markers[i]);
+		  });
+				i++;
 	})
 
-/**
-	for(var i = 0; i < data.length; i++){
-		var json = $.parseJSON(data.all_farmers[i]);
-		markers[i] = new google.maps.Marker({position : new google.maps.LatLng(json.latitude, json.longitude)});
-	}
-**/
-
-	var mc = new MarkerClusterer(map, markers);
+	mc = new MarkerClusterer(map, markers);
 }
 
 
